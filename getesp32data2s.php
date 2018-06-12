d043b232c1a5ec9a255269714167016cec8d7ff6
OmIndia <omgopalani@yahoo.com>
2018-06-13 01:29:33 +0530

    third commit

--- getesp32data2s.php
+++ getesp32data2s.php
@@ -3,13 +3,15 @@
 //https://stackoverflow.com/questions/4779963/how-can-i-access-my-localhost-from-my-android-device?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 // Om - Jun 12 2018 19:28 - works-gets ESP32 data into MySQL on PC - need to test on Heroku
 
-$query="";
-$link = mysqli_connect("localhost","root","hariom","test");
-       if (mysqli_connect_error()){
-        die( "Connection error");
-        }
+//$query="";
+//$link = mysqli_connect("localhost","root","hariom","test");                 //Om - Jun 13 2018 - put inside while loop below so mysql connection is closed each time
+
+//       if (mysqli_connect_error()){
+//        die( "Connection error");
+//        }
 //$conn = stream_socket_server('tcp://127.0.0.1:1337');  // Om - 12th Jun 2018 works with getesp32datac.php
-$conn = stream_socket_server('tcp://192.168.43.118:1337');  // Om - from ipconfig command run on PC
+//$conn = stream_socket_server('tcp://192.168.43.118:1337');  // Om - from ipconfig command run on PC
+$conn = stream_socket_server('tcp://afternoon-dawn-44153.herokuapp.com:80'); 
 while ($socket = stream_socket_accept($conn)) {
     $pkt = stream_socket_recvfrom($socket, 1500, 0, $peer);
     if (false === empty($pkt)) {
@@ -17,7 +19,20 @@ while ($socket = stream_socket_accept($conn)) {
         parse_str($pkt); //Om - get $id, $ts and $val parts of message from client
     }
     fclose($socket);
-    $query = "INSERT INTO heroku_7dd6781232a16b8.ecgdata (`Id`, `Timestamp`,`Value`) 
+    $query="";
+    
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+ 
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    
+    $link = mysqli_connect($server, $username, $password, $db);
+    if (mysqli_connect_error()){
+            die( "Connection error");
+            }
+    $query = "INSERT INTO ecgdata (`Id`, `Timestamp`,`Value`) 
         VALUES ($id, $ts,$val)"; 
     /*$query = "INSERT INTO patientdata (`Id`, `Systolic`, `Diastolic`, `Sugar`, `Temp`) 
         VALUES ($id, $sys, $dia, $sugar, $temp);";  */
@@ -33,9 +48,9 @@ while ($socket = stream_socket_accept($conn)) {
         else  {
         echo -1;  //does not work Apr 23 2017
     }
-    
+    mysqli_close($link);
     usleep(10000); //100ms delay
 }
 stream_socket_shutdown($conn, \STREAM_SHUT_RDWR);
-mysqli_close($link);
+//mysqli_close($link);     //Om - moved into while loop above - 13 Jun 2018
 ?>
